e0c7ac50200872a5c7a7f1953f7c9bcd72142dbb
dean0machin0 <dean0machin00@gmail.com>
2023-12-12 15:09:54 -0800

    projects.json updated

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <a>
+        </a>
+      </div>
+      <h1>A Portfolio By Dean</h1>
+      <div className="card">
+        <button>
+        </button>
+        <p>
+        </p>
+      </div>
+      <p className="">
+      </p>
+    </>
+  )
+}
+
+export default App

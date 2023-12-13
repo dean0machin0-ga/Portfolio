6864b151ba3e2708780bb99626c3aa45fa83d238
dean0machin0 <dean0machin00@gmail.com>
2023-12-13 13:11:30 -0800

    Project link functional

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,36 +1,35 @@
 /* eslint-disable no-unused-vars */
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 function Projects(props) {
-    const [projects, setProjects] = useState(null)
-
+    const [projects, setProjects] = useState(null);
+    
     const getProjectsData = async () => {
-        const response = await fetch("./projects.json")
-        const data = await response.json()
-        setProjects(data)
-    }
+    const response = await fetch("./projects.json")
+    const data = await response.json()
+    setProjects(data);
+    };
 
-    useEffect(() => {
-        getProjectsData()
-    }, [] )
+    useEffect(() => { 
+        getProjectsData() 
+        }, []);
 
     const loaded = () => {
-        return ( 
-            projects.map((project) => (
-            <div key={project._id}>
-                <h1>{project.name}</h1>
-                <img src={project.image} />
-                <a href={project.git}>
-                    <button>Github</button>
-                </a>
-                <a href={project.live}>
-                    <button>live sites</button>
-                </a>
-            </div>
-        )))
-    }
-    
-    return loaded ? loaded() : <h1>Loading...</h1>
+    return projects.map((project) => (
+        <div key={project.name}>
+            <h1>{project.name}</h1>
+            <img src={project.image} />
+            <a href={project.git}>
+                <button>Github</button>
+            </a>
+            <a href={project.live}>
+                <button>live site</button>
+            </a>
+        </div>
+        ));
+    };
+
+    return projects ? loaded() : <h1>Loading...</h1>;
 }
 
-export default Projects
+export default Projects;

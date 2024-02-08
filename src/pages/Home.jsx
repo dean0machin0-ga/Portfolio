f9420a35d41f3fcf4c1c2968b714a72ca024bad7
dean0machin0 <dean0machin00@gmail.com>
2024-02-07 17:55:07 -0800

    Updated my about info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,28 @@
-/* eslint-disable no-unused-vars */
-function Home(props) {
-    return <h1>Home</h1>
+import { useEffect, useState } from "react"
+
+function Home() {
+    const [home, setHome] = useState(null)
+
+    const getHomeData = async () => {
+        const response = await fetch("./home.json")
+        const data = await response.json()
+        setHome(data)
+    } 
+
+    useEffect( () => { 
+        getHomeData() 
+        }, [] )
+
+        const loaded = () => (
+            <div>
+            <href>{ home.headshot }</href>
+            <h3>{ home.email }</h3>
+            <p>{ home.bio }</p>
+            </div>
+    )
+    
+    return home ? loaded() : <h1>Loading...</h1>
 }
+    
 
 export default Home

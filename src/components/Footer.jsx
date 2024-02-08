57f639a77cb6e834e3ff880b3312891c86bd44c4
dean0machin0 <dean0machin00@gmail.com>
2024-02-07 18:17:06 -0800

    Updated  footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 
 function Footer(props) {
-    return <h1>Footer</h1>
+    return <h1>All Rights Reserved, &copy; 2024 Dean&apos;s Digital Showcase &nbsp;</h1>
 }
 
 export default Footer
